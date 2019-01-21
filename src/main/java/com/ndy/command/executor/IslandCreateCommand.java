e18ea45e5eaef553d16d4e1547a8d8fdbc746c05
dynam0507 <skaeodud0507@naver.com>
2019-01-21 19:35:20 +0900

    Command Added

---
+++ src/main/java/com/ndy/command/executor/IslandCreateCommand.java
@@ -0,0 +1,28 @@
+package com.ndy.command.executor;
+
+import com.ndy.command.impl.AbstractCommandRamify;
+import com.ndy.island.generator.IslandGenerator;
+import com.ndy.island.generator.executer.Generator;
+import com.ndy.island.storage.IslandStorage;
+import org.bukkit.entity.Player;
+
+public class IslandCreateCommand extends AbstractCommandRamify {
+
+    public IslandCreateCommand() {
+        super("create", "생성");
+    }
+
+    @Override
+    public boolean execute(Object... objects) {
+        Player player = (Player) objects[0];
+
+        if(IslandStorage.getInstance().isCreated(player)) {
+            setMessage("§c이미 생성된 섬입니다.");
+            return false;
+        }
+
+        Generator generator = new Generator().addGenerateAble(new IslandGenerator(player));
+        generator.dispose();
+        return true;
+    }
+}

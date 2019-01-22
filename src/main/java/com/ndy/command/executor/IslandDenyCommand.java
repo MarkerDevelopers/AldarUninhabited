f06c2539553e0dc024ef484d30bf14602b69a914
남대영 <wsnam0507@naver.com>
2019-01-22 17:16:33 +0900

    Aldar Renewal Snapshot-1.0.0

---
+++ src/main/java/com/ndy/command/executor/IslandDenyCommand.java
@@ -0,0 +1,29 @@
+package com.ndy.command.executor;
+
+import com.ndy.command.impl.AbstractCommandRamify;
+import com.ndy.island.party.invite.IslandPartyInviteScheduler;
+import com.ndy.island.party.invite.storage.IslandInviteSchedulerStorage;
+import org.bukkit.entity.Player;
+
+public class IslandDenyCommand extends AbstractCommandRamify {
+
+    public IslandDenyCommand() {
+        super("거절", "deny");
+    }
+
+    @Override
+    public boolean execute(Object... objects) {
+        Player player = (Player) objects[0];
+        IslandPartyInviteScheduler scheduler = IslandInviteSchedulerStorage.getScheduler(player);
+
+        if(scheduler == null) {
+            player.sendMessage("&c당신은 받은 초대가 없습니다.");
+            return false;
+        }
+
+        scheduler.getInvite().accept(false);
+        scheduler.cancelTask();
+        player.sendMessage("&c거절하였습니다.");
+        return false;
+    }
+}
